d0ab445a6c1120cc469038079a4dc6d5d460ec4b
AntiProtection <95133738+AntiProtection@users.noreply.github.com>
2022-02-10 21:07:31 +0900

    Update libkms.cpp

--- libkms.cpp
+++ libkms.cpp
@@ -3,12 +3,11 @@
 
 bool _send(SOCKET sock, const char* data, int len)
 {
-	if (!sock.connected()) return false;
 	return len == sock.write(data, len);
 }
 bool _recv(SOCKET sock, char* data, int len)
 {
-	if (!sock.connected() || sock.available() < len) return false;
+	if (sock.available() < len) return false;
 #ifdef ESP32
 	return len == sock.read((uint8_t*)data, len);
 #elif defined(ESP8266)
